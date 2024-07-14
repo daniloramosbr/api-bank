5fb50f497305338ec23ef238c0cb9af567721dbb
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-14 18:57:03 -0300

    deploy

--- public/src/controllers/TransactionController.js
+++ public/src/controllers/TransactionController.js
@@ -8,29 +8,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = __importDefault(require("../../database"));
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 class TransactionController {
     get(request, response) {
         return __awaiter(this, void 0, void 0, function* () {
+            // try {
+            //   const {rows} = await pool.query(
+            //     "SELECT * FROM transaction"
+            //   );
+            //   response.status(200).json(rows);
+            // } catch (error) {
+            //   response.status(500).send(error);
+            // }
             try {
-                const { rows } = yield database_1.default.query("SELECT * FROM transaction");
-                response.status(200).json(rows);
+                const transactions = yield prisma.transaction.findMany();
+                response.status(200).json(transactions);
             }
             catch (error) {
                 response.status(500).send(error);
             }
-            // try {
-            //   const transactions = await prisma.transaction.findMany();
-            //   response.status(200).json(transactions)
-            // } catch (error) {
-            //   response.status(500).send(error)
-            // }
         });
     }
 }
--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -8,23 +8,23 @@ const prisma = new PrismaClient();
 class TransactionController {
   async get(request: Request, response: Response) {
 
-    try {
-      const {rows} = await pool.query(
-        "SELECT * FROM transaction"
-      );
-
-      response.status(200).json(rows);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-
-
     // try {
-    //   const transactions = await prisma.transaction.findMany();
-    //   response.status(200).json(transactions)
+    //   const {rows} = await pool.query(
+    //     "SELECT * FROM transaction"
+    //   );
+
+    //   response.status(200).json(rows);
     // } catch (error) {
-    //   response.status(500).send(error)
+    //   response.status(500).send(error);
     // }
+
+
+    try {
+      const transactions = await prisma.transaction.findMany();
+      response.status(200).json(transactions)
+    } catch (error) {
+      response.status(500).send(error)
+    }
 
 
 
