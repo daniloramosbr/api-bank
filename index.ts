e0bf34441e54e174f179cb80224cc3ec2c4e163c
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-14 11:48:58 -0300

    deploy

--- index.ts
+++ index.ts
@@ -3,22 +3,26 @@ import routes from './routes/routes';
 import { configDotenv } from 'dotenv';
 import Cors from 'cors'
 import { PrismaClient } from "@prisma/client"
+import ConnectDb from './database';
 
 const dotenv = configDotenv()
 
 const app = express();
 
+
 export const prisma = new PrismaClient()
 
-prisma.$connect()
-  .then(() => {
-    console.log('conectado ao banco de dados!');
-  })
-  .catch((error) => {
-    console.error('Erro ao conectar com o Prisma:', error.message);
-  })
+// prisma.$connect()
+//   .then(() => {
+//     console.log('conectado ao banco de dados!');
+//   })
+//   .catch((error) => {
+//     console.error('Erro ao conectar com o Prisma:', error.message);
+//   })
 
 app.use(express.json());
+
+ConnectDb()
 
 app.options('*', Cors())
 app.use((req, res, next) => {
@@ -32,6 +36,6 @@ app.use((req, res, next) => {
   });
 
 app.use(routes)
-app.listen(3000, () => {
+app.listen(1905, () => {
   console.log('Servidor rodando na porta 3000')
 })
