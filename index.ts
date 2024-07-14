08ecf83d2d5f6cd6f79c09561cebdeda69fb4ef1
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-14 12:04:08 -0300

    deploy

--- index.ts
+++ index.ts
@@ -36,6 +36,6 @@ app.use((req, res, next) => {
   });
 
 app.use(routes)
-app.listen(1905, () => {
-  console.log('Servidor rodando na porta 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT}`)
 })
