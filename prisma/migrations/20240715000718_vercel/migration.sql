4852f0f67c274696c1aec4c0ddd65229a498241b
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-14 21:08:52 -0300

    deploy

---
+++ prisma/migrations/20240715000718_vercel/migration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Transaction` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Valor` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Transaction";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "Valor";
+
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" SERIAL NOT NULL,
+    "send" TEXT NOT NULL,
+    "received" TEXT NOT NULL,
+    "valor" INTEGER NOT NULL,
+
+    CONSTRAINT "transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" VARCHAR(255),
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "valor" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "valor" INTEGER NOT NULL DEFAULT 2000,
+
+    CONSTRAINT "valor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "valor_user_key" ON "valor"("user");
