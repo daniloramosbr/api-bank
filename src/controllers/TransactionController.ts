8e871ec1c80d2fbf5da53221e9ce4c221dea8ba4
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-14 21:32:42 -0300

    deploy

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -3,13 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class TransactionController {
+  //  Retorna as últimas 3 transações cadastradas
+  
   async get(request: Request, response: Response) {
-
     try {
-      const transactions = await prisma.transaction.findMany();
-      response.status(200).json(transactions)
+      const transactions = await prisma.transaction.findMany({
+        orderBy: {
+          createdAt: 'desc', // Ordena pela data de criação em ordem decrescente
+        },
+        take: 3,
+      });
+      
+      response.status(200).json(transactions);
     } catch (error) {
-      response.status(500).send(error)
+      response.status(500).send(error);
     }
   }
 }
