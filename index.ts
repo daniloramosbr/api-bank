4b3b48c85b9e62136332d522753dfc72af07adc5
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2024-07-18 21:50:52 -0300

    deploy

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./src/routes/routes";
 import { configDotenv } from "dotenv";
 const dotenv = configDotenv();
+import Cors from 'cors'
 
 import { PrismaClient } from "@prisma/client";
 
@@ -10,6 +11,17 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+
+app.options('*', Cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.use(routes);
 app.listen(process.env.PORT, () => {
--- public/index.js
+++ public/index.js
@@ -7,10 +7,18 @@ const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./src/routes/routes"));
 const dotenv_1 = require("dotenv");
 const dotenv = (0, dotenv_1.configDotenv)();
+const cors_1 = __importDefault(require("cors"));
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.options('*', (0, cors_1.default)());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(routes_1.default);
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT}`);
